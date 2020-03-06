69ffdf6c40e23f7790801ac74d2e1c61f1bd0b66
Jose Paulo Cabral <josepaucabral@gmail.com>
2020-03-06 13:25:15 +0800

    Device registration is now implemented.

--- gas_detection_system/manage_devices/urls.py
+++ gas_detection_system/manage_devices/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
 
-app_name = "manage_devices"
+app_name = "manage-devices"
 
 urlpatterns = [
+    path('<int:acc_id>/manage/',views.Manage.as_view(),name='manage_prints'),
+    path('<int:acc_id>/register/',views.Register.as_view(),name='register_prints'),
 ]
--- gas_detection_system/manage_devices/views.py
+++ gas_detection_system/manage_devices/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views import View
+from django.db import connection
+from django.contrib.auth.hashers import make_password, check_password
+from operator import itemgetter
 
 # Create your views here.
+def dictfetchall(cursor): 
+    "Returns all rows from a cursor as a dict" 
+    desc = cursor.description 
+    return [
+            dict(zip([col[0] for col in desc], row)) 
+            for row in cursor.fetchall() 
+    ]
+
+class Manage(View):
+    def get(self, request, *args, **kwargs):
+        acc_id = self.kwargs['acc_id']
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM devices WHERE accounts_acc_id={}".format(acc_id))
+            devices=dictfetchall(cursor)
+        return render(request,template_name='manage_devices/manage.html',context={'devices':devices,'acc_id':acc_id})
+
+    def post(self, request, *args, **kwargs):
+        acc_id = self.kwargs['acc_id']
+        device_id = request.POST["device_id"]
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM devices WHERE device_id={}".format(device_id))
+        return redirect("/devices/{}/manage/".format(acc_id))
+
+class Register(View):
+    def get(self, request, *args, **kwargs):
+        acc_id = self.kwargs['acc_id']
+        return render(request,template_name='manage_devices/register.html',context={'acc_id':acc_id})
+
+    def post(self, request, *args, **kwargs):
+        acc_id = self.kwargs['acc_id']
+        device_id = request.POST["device_id"]
+        room = request.POST["room"]
+        with connection.cursor() as cursor:
+            cursor.execute("INSERT INTO devices VALUES({},'{}',{})".format(device_id, room, acc_id))
+        return redirect("/devices/{}/manage/".format(acc_id))
+
+        
