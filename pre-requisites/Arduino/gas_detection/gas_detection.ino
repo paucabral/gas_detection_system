51e9b7e6661da8ca36a12b3d78e6148a32bb4c55
Janrey Licas <janjan.licas@gmail.com>
2020-03-09 16:17:21 +0800

    Gas Detection Sketch | Added LCD I2C and Added More Stuff

--- pre-requisites/Arduino/gas_detection/gas_detection.ino
+++ pre-requisites/Arduino/gas_detection/gas_detection.ino
@@ -2,95 +2,113 @@
 #include "SIM900.h"
 #include "sms.h"
 #include <MQ2.h>
+#include "LiquidCrystal_I2C.h"
 
-int Analog_Input = A0;
-int ledReady = 8;
+const int Analog_Input = A0, ledReady = 8, pinBuzzer = ;
 int lpg, co, smoke;
 MQ2 mq2(Analog_Input);
 
 SMSGSM sms;
+LiquidCrystal_I2C LCD_DRVR(0x27, 16, 2); // Declare LCD Screen
+SoftwareSerial wifiSerial(2, 3);         // RX, TX for ESP8266
 
-SoftwareSerial wifiSerial(2, 3);      // RX, TX for ESP8266
-
-bool DEBUG = true;   //show more logs
+bool DEBUG = true;     //show more logs
 int responseTime = 10; //communication timeout
 
 int device_id = 69;
 String PostMessage = "";
 
 int numdata;
-boolean started=false;
+boolean started = false;
 char smsbuffer[160];
 char n[20];
 
 void setup()
 {
-  //ledReadyStatus
+  Serial.begin(115200);
+  LCD_DRVR.begin();
+
+  // Setup Pins
   pinMode(ledReady, OUTPUT);
-  
+  while (!Serial)
+    ;
+
+  LCD_DRVR.setCursor(0, 0);
+  LCD_DRVR.print(F(" Gas Detection  "));
+  LCD_DRVR.setCursor(0, 1);
+  LCD_DRVR.print(F("  For CpEHacks  "));
+
+  LCD_DRVR.noBacklight();
+  LCD_DRVR.clear();
+  delay(500);
+  LCD_DRVR.backlight();
+
   //Start Wifi Module
   // Open serial communications and wait for port to open esp8266:
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
   wifiSerial.begin(115200);
 
-  sendToWifi("AT+RST",responseTime,DEBUG);
+  sendToWifi("AT+RST", responseTime, DEBUG);
   //Serial.println("AT+RST");
-  
-  sendToWifi("AT+CWJAP=\"cabral\",\"17151613\"",responseTime,DEBUG);
+
+  sendToWifi("AT+CWJAP=\"RNHLD\",\"MotivationsPLS\"", responseTime, DEBUG);
   //Serial.println("AT+CWJAP=\"cabral\",\"17151613\"");
   delay(4000);
-  
-  sendToWifi("AT+CWJAP?",responseTime,DEBUG);
+
+  sendToWifi("AT+CWJAP?", responseTime, DEBUG);
   //Serial.println("AT+CWJAP?");
   delay(4000);
-  
-  sendToWifi("AT+CIFSR",responseTime,DEBUG);
+
+  sendToWifi("AT+CIFSR", responseTime, DEBUG);
   //Serial.println("AT+CIFSR");
   delay(1000);
-  
-  sendToWifi("AT+CIPMUX=1",responseTime,DEBUG);
+
+  sendToWifi("AT+CIPMUX=1", responseTime, DEBUG);
   //Serial.println("AT+CIPMUX=1");
   delay(4000);
-  
-  sendToUno("Wifi connection is running!",responseTime,DEBUG);
+
+  sendToUno("Wifi connection is running!", responseTime, DEBUG);
   //End Wifi Module
 
   //Start GSM Module
-  if (gsm.begin(2400)){
+  if (gsm.begin(2400))
+  {
     Serial.println("\nstatus=READY");
-    started=true;  
+    started = true;
   }
-  else Serial.println("\nstatus=IDLE");
+  else
+    Serial.println("\nstatus=IDLE");
   //End GSM Module
 
   //Start GSM Module
-  if (gsm.begin(2400)){
+  if (gsm.begin(2400))
+  {
     Serial.println("\nstatus=READY");
-    started=true;  
+    started = true;
   }
-  else Serial.println("\nstatus=IDLE");
+  else
+    Serial.println("\nstatus=IDLE");
   //End GSM Module
-  
+
   digitalWrite(ledReady, HIGH);
 }
 
 //Text function
-void text(){
-  if(started){
+void text()
+{
+  if (started)
+  {
     if (sms.SendSMS("09953148842", "Alert! Gas particle/s has been detected. Please check the web application for details."))
       Serial.println("\nSMS sent OK");
-  }  
+  }
 }
 //end text function
 
 //post
-void post(String gases, int smokePPM){
+void post(String gases, int smokePPM)
+{
   String gas = gases;
   int smoke = smokePPM;
-  
+
   PostMessage += "{\"device_id\":";
   PostMessage += "\"";
   PostMessage += device_id;
@@ -105,39 +123,37 @@ void post(String gases, int smokePPM){
   PostMessage += "\"";
   PostMessage += "}";
 
-  unsigned int l=PostMessage.length();
-  
-  String JSON = "";
-  
-  JSON += "POST /post/notif HTTP/1.1\n";
+  unsigned int l = PostMessage.length();
 
-  JSON += "Host:192.168.254.107:5000\n";
+  String JSON = "";
+
+  JSON += "POST /post/notif HTTP/1.1\n";
+  JSON += "Host:192.168.43.90:5000\n";
 
   JSON += "Content-Length:";
   JSON += l;
   JSON += "\n";
 
   JSON += "Content-Type:application/json\n\n";
-  
+
   JSON += PostMessage;
 
-  unsigned int i=JSON.length();
+  unsigned int i = JSON.length();
 
-  
-  sendToUno(JSON,responseTime,DEBUG);
+  sendToUno(JSON, responseTime, DEBUG);
   String len = "";
   len += i;
-  sendToUno(len,responseTime,DEBUG);
+  sendToUno(len, responseTime, DEBUG);
 
   String startMsg = "";
   startMsg += "AT+CIPSTART=0,";
   startMsg += "\"TCP\",";
-  startMsg += "\"192.168.254.107\",";
+  startMsg += "\"192.168.43.90\",";
   startMsg += "5000";
   sendToWifi(startMsg, responseTime, DEBUG);
   startMsg = "";
   delay(4000);
-  
+
   String cipMsg = "";
   cipMsg += "AT+CIPSEND=0,";
   cipMsg += i;
@@ -146,7 +162,7 @@ void post(String gases, int smokePPM){
   delay(4000);
 
   sendToWifi(JSON, responseTime, DEBUG);
-  
+
   String closeMsg = "";
   closeMsg += "AT+CIPCLOSE=0,";
   sendToWifi(closeMsg, responseTime, DEBUG);
@@ -157,8 +173,9 @@ void post(String gases, int smokePPM){
 }
 //end post
 
-void gas(){
-  float* values= mq2.read(false); //set it false if you don't want to print the values in the Serial
+void gas()
+{
+  float *values = mq2.read(false); //set it false if you don't want to print the values in the Serial
   //lpg = values[0];
   lpg = mq2.readLPG();
   //co = values[1];
@@ -166,107 +183,115 @@ void gas(){
   //smoke = values[2];
   smoke = mq2.readSmoke();
 
-  if(lpg == 0 && co == 0){
+  if (lpg == 0 && co == 0)
+  {
     //put lcd display of normal
     post("Normal", smoke);
     sendToUno("Normal Reading", responseTime, DEBUG);
     delay(30000);
   }
-  else if(lpg > 0 && co == 0){
+  else if (lpg > 0 && co == 0)
+  {
     //put lcd alert
     post("LPG", smoke);
     text();
   }
-  else if(co > 0 && lpg == 0){
+  else if (co > 0 && lpg == 0)
+  {
     //put lcd alert
     post("CO", smoke);
     text();
   }
-  else if(co > 0 && lpg > 0){
+  else if (co > 0 && lpg > 0)
+  {
     //put lcd alert
     post("CO and LPG", smoke);
     text();
   }
 }
 
-void loop(){
+void loop()
+{
+  LCD_DRVR.setCursor(0, 0);
+  LCD_DRVR.print(F("Status |> "));
+  LCD_DRVR.print();
+  LCD_DRVR.setCursor(0, 1);
+  LCD_DRVR.print(F("LPG"));
   String gas = "";
 
-  //lpg = values[0];
   lpg = mq2.readLPG();
-  //co = values[1];
   co = mq2.readCO();
-  //smoke = values[2];
   smoke = mq2.readSmoke();
 
-  if(lpg == 0 && co == 0){
+  if (lpg == 0 && co == 0)
+  {
     //put lcd display of normal
     post("Normal", smoke);
   }
-  else if(lpg > 0 && co == 0){
+  else if (lpg > 0 && co == 0)
+  {
     //put lcd alert
     //post("LPG", smoke);
     //text();
-    delay(1000);
-    gas += "LPG";
+    post("LPG", smoke);
   }
-  else if(co > 0 && lpg == 0){
+  else if (co > 0 && lpg == 0)
+  {
     //put lcd alert
     //post("CO", smoke);
     //text();
-    delay(1000);
-    gas += "CO";
+    post("CO", smoke);
   }
-  else if(co > 0 && lpg > 0){
+  else if (co > 0 && lpg > 0)
+  {
     //put lcd alert
     //post("CO and LPG", smoke);
     //text();
-    gas += "CO and LPG";
+    post("CO and LPG", smoke);
   }
-
-  post(gas, smoke);
 }
-
 
 /*
 * Name: sendData
 * Description: Function used to send string to tcp client using cipsend
-* Params: 
+* Params:
 * Returns: void
 */
-void sendData(String str){
-  String len="";
-  len+=str.length();
-  sendToWifi("AT+CIPSEND=0,"+len,responseTime,DEBUG);
+void sendData(String str)
+{
+  String len = "";
+  len += str.length();
+  sendToWifi("AT+CIPSEND=0," + len, responseTime, DEBUG);
   delay(100);
-  sendToWifi(str,responseTime,DEBUG);
+  sendToWifi(str, responseTime, DEBUG);
   delay(100);
-  sendToWifi("AT+CIPCLOSE=5",responseTime,DEBUG);
+  sendToWifi("AT+CIPCLOSE=5", responseTime, DEBUG);
 }
-
 
 /*
 * Name: find
 * Description: Function used to match two string
-* Params: 
+* Params:
 * Returns: true if match else false
 */
-boolean find(String string, String value){
-  return string.indexOf(value)>=0;
+boolean find(String string, String value)
+{
+  return string.indexOf(value) >= 0;
 }
-
 
 /*
 * Name: readSerialMessage
 * Description: Function used to read data from Arduino Serial.
-* Params: 
+* Params:
 * Returns: The response from the Arduino (if there is a reponse)
 */
-String  readSerialMessage(){
-  char value[100]; 
-  int index_count =0;
-  while(Serial.available()>0){
-    value[index_count]=Serial.read();
+String readSerialMessage()
+{
+  char value[100];
+  int index_count = 0;
+  while (Serial.available() > 0)
+  {
+    value[index_count] = Serial.read();
     index_count++;
     value[index_count] = '\0'; // Null terminate the string
   }
@@ -274,20 +299,20 @@ String  readSerialMessage(){
   str.trim();
   return str;
 }
-
-
 
 /*
 * Name: readWifiSerialMessage
 * Description: Function used to read data from ESP8266 Serial.
-* Params: 
+* Params:
 * Returns: The response from the esp8266 (if there is a reponse)
 */
-String  readWifiSerialMessage(){
-  char value[100]; 
-  int index_count =0;
-  while(wifiSerial.available()>0){
-    value[index_count]=wifiSerial.read();
+String readWifiSerialMessage()
+{
+  char value[100];
+  int index_count = 0;
+  while (wifiSerial.available() > 0)
+  {
+    value[index_count] = wifiSerial.read();
     index_count++;
     value[index_count] = '\0'; // Null terminate the string
   }
@@ -295,8 +320,6 @@ String  readWifiSerialMessage(){
   str.trim();
   return str;
 }
-
-
 
 /*
 * Name: sendToWifi
@@ -304,20 +327,21 @@ String  readWifiSerialMessage(){
 * Params: command - the data/command to send; timeout - the time to wait for a response; debug - print to Serial window?(true = yes, false = no)
 * Returns: The response from the esp8266 (if there is a reponse)
 */
-String sendToWifi(String command, const int timeout, boolean debug){
+String sendToWifi(String command, const int timeout, boolean debug)
+{
   String response = "";
   wifiSerial.println(command); // send the read character to the esp8266
   long int time = millis();
-  while( (time+timeout) > millis())
+  while ((time + timeout) > millis())
   {
-    while(wifiSerial.available())
+    while (wifiSerial.available())
     {
-    // The esp has data so display its output to the serial window 
-    char c = wifiSerial.read(); // read the next character.
-    response+=c;
-    }  
+      // The esp has data so display its output to the serial window
+      char c = wifiSerial.read(); // read the next character.
+      response += c;
+    }
   }
-  if(debug)
+  if (debug)
   {
     Serial.println(response);
   }
@@ -330,20 +354,21 @@ String sendToWifi(String command, const int timeout, boolean debug){
 * Params: command - the data/command to send; timeout - the time to wait for a response; debug - print to Serial window?(true = yes, false = no)
 * Returns: The response from the esp8266 (if there is a reponse)
 */
-String sendToUno(String command, const int timeout, boolean debug){
+String sendToUno(String command, const int timeout, boolean debug)
+{
   String response = "";
   Serial.println(command); // send the read character to the esp8266
   long int time = millis();
-  while( (time+timeout) > millis())
+  while ((time + timeout) > millis())
   {
-    while(Serial.available())
+    while (Serial.available())
     {
-      // The esp has data so display its output to the serial window 
+      // The esp has data so display its output to the serial window
       char c = Serial.read(); // read the next character.
-      response+=c;
-    }  
+      response += c;
+    }
   }
-  if(debug)
+  if (debug)
   {
     Serial.println(response);
   }
